17d02fb8c94b8afd4ffc5706525371acab2a09e9
nisxl <nis.maharaj@gmail.com>
2024-03-10 20:57:29 +0545

    ml chatbot gemini

---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+# see the env variable 
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+
+    return text
+
+def get_text_chunks(text):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store=FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def collect_user_info():
+    st.subheader("Please provide your contact information:")
+    name = st.text_input("Name:")
+    phone_number = st.text_input("Phone Number:")
+    email = st.text_input("Email:")
+
+    return name, phone_number, email
+
+
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model=ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    
+    PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+    chain=load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    # Add the form to collect user information
+    name, phone_number, email = collect_user_info()
+
+    if st.button("Call me"):
+        if name and phone_number and email:
+            st.write(f"Thank you, {name}! We will call you at {phone_number} or reach out via email at {email}.")
+        else:
+            st.write("Please provide your contact information before requesting a call.")
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+if __name__ == "__main__":
+    main()
